f78f098cb231fad47d84c84196b3ec54acf0dc63
Morning <morning@mstars-lab2-10s-iMac.local>
2023-03-30 09:32:36 +0800

    fixing .env

--- e-commerce-backend/src/app.js
+++ e-commerce-backend/src/app.js
@@ -3,18 +3,23 @@ console.log("iShop E-Commerce Backend");
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv"
 import menuApi from "./routes/menu-api";
 import adminApi from "./routes/admin-api";
 
+dotenv.config()
+
 const app = express();
-const PORT = 8082;
-const MONGODB_CONNECTION_STRING =
-  "mongodb+srv://blackbilguun:TEozn8OuZBEU4XQF@bilguunee.rn8hbjv.mongodb.net/ishop";
+const PORT = process.env.PORT;
+const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
 
 app.use(cors());
 app.use(express.json());
 app.use("/menu", menuApi);
 app.use("/admin", adminApi);
+app.get('/', (req, res) => {
+  res.send('<h1>Day - 88</h1>')
+})
 
 app.listen(PORT, () => {
   mongoose
